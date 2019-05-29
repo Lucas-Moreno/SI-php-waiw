aa0b9fdd8eb1ef7f92ae09a2e30d22a7565463c1
Clement <clement.sabourin@hetic.net>
2019-05-29 11:09:41 +0200

    Avant le merge de Sabou sur master

---
+++ src/assets/inc/entete.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="scss/reset.scss">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../src/css/style.css">
+  <title>WAIW</title>
+</head>
---
+++ src/inscription.php
@@ -0,0 +1,12 @@
+<?php
+  include_once('assets/inc/entete.php');
+?>
+<body>
+<?php
+  include_once('assets/inc/banniere.php');
+  //forlulaire
+  include_once('assets/inc/form.php');
+?>
+
+</body>
+</html>
